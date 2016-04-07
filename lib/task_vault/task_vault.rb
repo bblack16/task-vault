17f7222e6385158ba126970d2adfd8af794ac560
Brandon <d2sm10@hotmail.com>
2016-04-06 22:49:09 -0600

    Removed Sentry from stop method. Added new methods to get server info.

--- lib/task_vault/task_vault.rb
+++ lib/task_vault/task_vault.rb
@@ -4,17 +4,17 @@ require_relative "vault/vault"
 require_relative "workbench/workbench"
 require_relative "protectron/protectron"
 require_relative "courier/courier"
-require_relative "overworld/overworld"
+require_relative "sentry/sentry"
 
 class TaskVault
-  attr_reader :cfg_policy, :path, :vault, :workbench, :protectron, :courier, :overworld
+  attr_reader :cfg_policy, :path, :vault, :workbench, :protectron, :courier, :sentry
 
   def initialize path = Dir.pwd, start: false
     @vault = Vault.new
     @workbench = Workbench.new(self)
     @courier = Courier.new(self)
-    @overworld = Overworld.new(self)
-    @protectron = Protectron.new self
+    @sentry = Sentry.new(self)
+    @protectron = Protectron.new(self)
     self.path = path
     self.start if start
   end
@@ -33,24 +33,81 @@ class TaskVault
     @vault.start
     @courier.start
     @workbench.start
-    @overworld.start
+    @sentry.start
     @protectron.start
+    running?
   end
 
   def stop
     @protectron.stop
-    @overworld.stop
+    # @sentry.stop # For now this won't get stopped because it is essential for communication
     @workbench.stop
     @vault.stop
     @courier.stop
+    !running?
   end
 
   def restart
     @vault.restart
     @workbench.restart
     @courier.restart
-    @overworld.restart
+    @sentry.restart
     @protectron.restart
+    running?
+  end
+
+  def running?
+    @vault.running? && @workbench.running? && @courier.running? && @protectron.running? && @sentry.running?
+  end
+
+  def health
+    if running?
+      :green
+    elsif @vault.running? || @workbench.running? || @courier.running?
+      :yellow
+    else
+      :red
+    end
+  end
+
+  def status
+    {
+      ip_address: ip_address,
+      health: health,
+      components: component_status,
+      time: server_time,
+      running: running?
+    }
+  end
+
+  def component_status
+    {
+      vault: {running: @vault.running?, uptime: @vault.uptime},
+      workbench: {running: @workbench.running?, uptime: @workbench.uptime},
+      courier: {running: @courier.running?, uptime: @courier.uptime},
+      protectron: {running: @protectron.running?, uptime: @protectron.uptime},
+      sentry: {running: @sentry.running?, uptime: @sentry.uptime},
+      message_handlers: @courier.handlers.map{ |h| [h.name, {running: h.running?, uptime: h.uptime}]}.to_h
+    }
+  end
+
+  def ip_address version = 4
+    ips = Socket.ip_address_list.map{ |i| i.ip_address }
+    case version
+    when 4 || :v4 || :ipv4
+      ips.reject!{|r| !(r =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/) || r == '127.0.0.1'}
+    when 6 || :v6 || :ipv6
+      ips.reject!{|r| (r =~ /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/) || r == '::1'}
+    when 'all' || :all
+      # nada to do
+    else
+      return nil
+    end
+    ips
+  end
+
+  def server_time
+    Time.now
   end
 
 end
