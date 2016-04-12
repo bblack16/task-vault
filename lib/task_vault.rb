403e18dd9a1a2b55b9c4195ce39d79fd875ee2f2
Brandon <d2sm10@hotmail.com>
2016-04-11 23:26:57 -0600

    Added require for ava

--- lib/task_vault.rb
+++ lib/task_vault.rb
@@ -1,10 +1,12 @@
+require 'json'
+require 'yaml'
+require 'bblib' if !defined?(BBLib::VERSION)
+require 'ava'
+
 require_relative "task_vault/version"
 require_relative "task_vault/task_vault"
 require_relative "pipboy/pipboy"
 
-require 'json'
-require 'yaml'
-require 'bblib' if !defined?(BBLib::VERSION)
 
 
 class TaskVault
