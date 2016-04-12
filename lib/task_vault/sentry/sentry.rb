f7090e7bd5b38fcbae78f59bf8c0f34b4f568cde
Brandon <d2sm10@hotmail.com>
2016-04-11 23:27:22 -0600

    Sentry is now a wrapper of an Ava::Controller.

--- lib/task_vault/sentry/sentry.rb
+++ lib/task_vault/sentry/sentry.rb
@@ -1,78 +1,59 @@
 require 'socket'
-require_relative 'overseer'
 
 class TaskVault
 
   class Sentry < Component
-    attr_reader :mode, :port
+    attr_reader :port, :controller, :key
 
-    def mode= m
-      raise ArgumentError, "Invalid Sentry mode '#{m}'. Options are #{MODES.join(', ')}" unless MODES.include?(m)
-      @mode = m
-      restart if running?
+    def running?
+      @controller.running?
+    end
+
+    def start
+      @controller.start
+    end
+
+    def stop
+      @controller.stop
+    end
+
+    def restart
+      @controller.restart
     end
 
     def port= port
       @port = port.to_i
-      restart if running?
+      @controller.port = @port
+      @controller.restart
+    end
+
+    def key= key
+      @key = key.to_s
+      @controller.key = @key
+      @controller.restart
     end
 
     protected
 
-      MODES = [:tcp, :udp]
-
       def setup_defaults
-        self.port = 2016
-        self.mode = :tcp
+        @port = 2016
+        @key = 'changeme'
+        @controller = Ava::Controller.new(port: @port, key: @key)
+        @controller.register(
+          task_vault: @parent,
+          vault: @parent.vault,
+          courier: @parent.courier,
+          protectron: @parent.protectron,
+          workbench: @parent.workbench,
+          sentry: self
+        )
       end
 
       def init_thread
-        send("init_#{@mode}".to_sym)
+        @controller.start
+        queue_msg("INFO - Sentry is up and listening on port #{@port}.")
       end
 
-      def init_tcp
-        @thread = Thread.new {
-          begin
-            server = TCPServer.new(@port)
-            queue_msg("INFO - Sentry is up and listening to TCP on port #{@port}.")
-            loop do
-              Thread.start(server.accept) do |client|
-                sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
-                msg = client.gets
-                overseer = Overseer.new(@parent)
-                result = overseer.handle_request(msg)
-                client.puts result.to_json
-                if result[:erorr].nil?
-                  queue_msg("DEBUG - Sentry spotted a reqest from #{remote_hostname} (#{remote_ip}) and fulfilled it.", :default)
-                else
-                  queue_msg("WARN - Sentry spotted a reqest from #{remote_hostname} (#{remote_ip}) and failed to process it. First 150 chars of error: #{result[:error][0..149]}", :default)
-                end
-                client.close
-              end
-            end
-          rescue StandardError, Exception => e
-            queue_msg(e)
-            e
-          end
-        }
-      end
-
-      def init_udp
-        @thread = Thread.new {
-          begin
-            server = UDPSocket.new
-            server.bind('localhost', @port)
-            queue_msg("INFO - Sentry is up and listening to UDP on port #{@port}.")
-            loop do
-              text, sender = s.recvfrom(16)
-              queue_msg(text, sender:sender)
-            end
-          rescue StandardError, Exception => e
-            queue_msg(e)
-            e
-          end
-        }
-      end
-  end
+    end
 
 end
