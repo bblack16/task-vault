69c178a8ba7d125e96208ca2d9ea04d488f4939d
U-EWICO\bblack <bblack@DGCH8N8V1.digitalglobe.com>
2016-04-14 13:48:38 -0600

    Changed misnamed variable for message_handlers.

--- lib/task_vault/task/dynamic_tasks/dynamic_scripts.rb
+++ lib/task_vault/task/dynamic_tasks/dynamic_scripts.rb
@@ -4,9 +4,9 @@ class TaskVault
     attr_reader :path, :filter
     attr_accessor :recursive
     attr_accessor :working_dir, :args, :weight, :priority,
-                  :max_life, :value_cap, :repeat, :delay, :message_handler_name
+                  :max_life, :value_cap, :repeat, :delay, :message_handlers
 
-    def initialize name:nil, path:Dir.pwd, filter: '*', recursive: false, working_dir:nil, args:nil, weight:1, priority:3, max_life:nil, value_cap:1000000, repeat:1, delay:0, message_handler_name: :default
+    def initialize name:nil, path:Dir.pwd, filter: '*', recursive: false, working_dir:nil, args:nil, weight:1, priority:3, max_life:nil, value_cap:1000000, repeat:1, delay:0, message_handlers: :default
       super(name)
       self.path = path
       self.filter = filter
@@ -19,7 +19,7 @@ class TaskVault
       self.value_cap = value_cap
       self.repeat = repeat
       self.delay = delay
-      self.message_handler_name = message_handler_name
+      self.message_handlers = message_handlers
     end
 
     def path= path
