bb43cc1a26f4cf48d54d40e30c130ed8f9c89de9
Brandon <d2sm10@hotmail.com>
2016-05-15 21:20:53 -0600

    Refactoring started to work as a standard task. Still in progress.

--- lib/task_vault/task/dynamic_tasks/dynamic_scripts.rb
+++ lib/task_vault/task/dynamic_tasks/dynamic_scripts.rb
@@ -2,25 +2,8 @@ class TaskVault
 
   class DynamicScripts < DynamicTask
     attr_reader :path, :filter
-    attr_accessor :recursive
-    attr_accessor :working_dir, :args, :weight, :priority,
+    attr_accessor :recursive, :working_dir, :args, :weight, :priority,
                   :max_life, :value_cap, :repeat, :delay, :message_handlers
-
-    def initialize name:nil, path:Dir.pwd, filter: '*', recursive: false, working_dir:nil, args:nil, weight:1, priority:3, max_life:nil, value_cap:1000000, repeat:1, delay:0, message_handlers: :default
-      super(name)
-      self.path = path
-      self.filter = filter
-      self.recursive = recursive
-      self.working_dir = working_dir
-      self.args = args
-      self.weight = weight
-      self.priority = priority
-      self.max_life = max_life
-      self.value_cap = value_cap
-      self.repeat = repeat
-      self.delay = delay
-      self.message_handlers = message_handlers
-    end
 
     def path= path
       @path = path
@@ -31,12 +14,28 @@ class TaskVault
     end
 
     def generate_tasks
-      tasks = []
-      BBLib.scan_files(@path, filter:@filter, recursive:@recursive).each do |script|
-        tasks.push Task.new(name:"#{@name}_#{script}", type: :script, job:script, working_dir:@working_dir, args:@args, weight:@weight, priority:@priority, max_life:@max_life, value_cap:@value_cap, repeat:@repeat, delay:@delay, message_handlers:@message_handlers)
+      BBLib.scan_files(@path, filter:@filter, recursive:@recursive).map do |script|
+        Task.new(name:"#{@name}_#{script}", type: :script, cmd:script, working_dir:@working_dir, args:@args, weight:@weight, priority:@priority, max_life:@max_life, value_cap:@value_cap, repeat:@repeat, delay:@delay, message_handlers:@message_handlers)
       end
-      return tasks
     end
+    
+    protected
+    
+      def setup_defaults
+        super
+        self.path = Dir.pwd
+        self.filter = '*'
+        self.recursive = false
+        self.working_dir = nil
+        self.args = nil
+        self.weight = 1
+        self.priority = 3
+        self.max_life = nil
+        self.value_cap = 1000000
+        self.repeat = 1
+        self.delay = 0
+        self.message_handlers = :default
+      end
 
   end
 
