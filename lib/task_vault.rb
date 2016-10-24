e8eec766a16234bf5925af88e35acb50dea65eaf
Brandon <d2sm10@hotmail.com>
2016-10-23 23:14:43 -0600

    Added namespace crawler to compile available task classes.

--- lib/task_vault.rb
+++ lib/task_vault.rb
@@ -10,4 +10,27 @@ require_relative 'tasks/_tasks'
 
 module TaskVault
 
+  def self.registry
+    @@registry ||= Array.new
+  end
+
+  singleton_class.send(:alias_method, :task_types, :registry)
+
+  def self.load_registry *namespaces
+    registry.clear
+    namespaces = [TaskVault] if namespaces.empty?
+    namespaces.each do |namespace|
+      namespace.constants.each do |constant|
+        constant = namespace.const_get(constant.to_s)
+        next if constant == TaskVault::Task
+        if constant.respond_to?(:is_task_vault_task?) && constant.is_task_vault_task?
+          registry.push(constant) unless registry.include?(constant)
+        end
+      end
+    end
+    registry
+  end
+
 end
+
+TaskVault.load_registry
