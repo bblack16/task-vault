9c03a2dc3d381d4995e926ea882a34318941b31c
Brandon <d2sm10@hotmail.com>
2017-02-13 22:48:32 -0700

    Added additional routes and tweaked some behavior.

--- lib/components/wasteland/components/wasteland_component.rb
+++ lib/components/wasteland/components/wasteland_component.rb
@@ -17,13 +17,12 @@ module TaskVault
       @params = params
     end
 
+    protected
+
     def add_route(verb, path, &block)
       raise ArgumentError, "Unknown http verb '#{verb}'. Verb must be of #{VERBS.join(', ')}" unless VERBS.include?(verb)
-      (routes[verb] ||= {})[path] = block
-    end
-
-    def call_route(verb, path, *args)
-      routes[verb][path].call(*args)
+      full_route = "/components/#{name}#{path}"
+      Wasteland::Server.send(verb, full_route, &block) unless Wasteland::Server.route_names(verb).include?(full_route)
     end
 
     def get(path, &block)
@@ -42,16 +41,44 @@ module TaskVault
       add_route(:delete, path, &block)
     end
 
-    protected
-
     def setup_routes
       get '/' do
-        self.serialize.merge(
-          uptime: uptime,
-          started: started,
-          stopped: stopped,
-          history: history[0..4].map { |h| "#{h[:time]} - #{h[:severity].to_s.upcase} - #{h[:msg]}" }
+        component.serialize.merge(
+          description: component.description,
+          uptime:      component.uptime,
+          started:     component.started,
+          stopped:     component.stopped,
+          history:     component.history[0..4].map { |h| "#{h[:time]} - #{h[:severity].to_s.upcase} - #{h[:msg]}" }
         )
+      end
+
+      get '/settings' do
+        component.serialize
+      end
+
+      post '/settings' do
+        params.map do |k, v|
+          if component.respond_to?("#{k}=")
+            component.send("#{k}=", *[v].flatten(1))
+            [k, component.send(k)]
+          else
+            [k, nil]
+          end
+        end.to_h
+      end
+
+      get '/logs' do
+        process_component_logs(component.history)
+      end
+
+      get '/message_queue' do
+        component.message_queue
+      end
+
+      [:start, :stop, :restart].each do |cmd|
+        put "/#{cmd}" do
+          { cmd => component.send(cmd) }
+        end
       end
     end
 
