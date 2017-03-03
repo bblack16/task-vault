cafdeeca3c02c85249c89e42239109f6fe4bf0a8
Brandon <d2sm10@hotmail.com>
2017-03-02 23:03:42 -0700

    Moved tasks into the Tasks module for cleaner namespaces.

--- lib/components/vault/tasks/tasks/cmd_task.rb
+++ lib/components/vault/tasks/tasks/cmd_task.rb
@@ -2,48 +2,52 @@
 require 'open3'
 
 module TaskVault
-  class CMDTask < Task
-    attr_string :command, default: '', serialize: true, always: true
-    attr_valid_dir :working_directory, allow_nil: true, serialize: true, always: true, pre_proc: proc { |x| x.to_s.empty? ? nil : x }
-    attr_array :arguments, default: [], serialize: true, always: true
+  module Tasks
+    class CMDTask < Task
+      attr_string :command, default: '', serialize: true, always: true
+      attr_valid_dir :working_directory, allow_nil: true, serialize: true, always: true, pre_proc: proc { |x| x.to_s.empty? ? nil : x }
+      attr_array :arguments, default: [], serialize: true, always: true
 
-    alias cmd= command=
-    alias cmd command
+      alias cmd= command=
+      alias cmd command
 
-    alias args= arguments=
-    alias args arguments
+      alias args= arguments=
+      alias args arguments
 
-    protected
+      add_alias(:cmd, :cmd_task)
 
-    def run
-      command = compile_cmdline
-      queue_msg("About to run cmd: #{command}", severity: :debug)
-      Open3.popen3(command, chdir: (@working_directory || '/')) do |_i, o, e, w|
-        @pid = w.pid
-        [o, e].each do |stream|
-          stream.each do |line|
-            queue_msg(line, severity: (stream == o ? :info : :error))
+      protected
+
+      def run
+        command = compile_cmdline
+        queue_msg("About to run cmd: #{command}", severity: :debug)
+        Open3.popen3(command, chdir: (@working_directory || '/')) do |_i, o, e, w|
+          @pid = w.pid
+          [o, e].each do |stream|
+            stream.each do |line|
+              queue_msg(line, severity: (stream == o ? :info : :error))
+            end
           end
         end
       end
-    end
 
-    def compile_cmdline
-      cmd = @arguments.map do |a|
-        if a.include?(' ') && !a.encap_by?('"')
-          "\"#{a.gsub('"', '\\"')}\""
-        else
-          a
+      def compile_cmdline
+        cmd = @arguments.map do |a|
+          if a.include?(' ') && !a.encap_by?('"')
+            "\"#{a.gsub('"', '\\"')}\""
+          else
+            a
+          end
         end
+        cmd.unshift(@command)
+        cmd.join(' ')
       end
-      cmd.unshift(@command)
-      cmd.join(' ')
-    end
 
-    def msg_metadata
-      {
-        pid: @pid || nil
-      }.merge(super)
+      def msg_metadata
+        {
+          pid: @pid || nil
+        }.merge(super)
+      end
     end
   end
 end
--- lib/components/vault/tasks/tasks/eval_task.rb
+++ lib/components/vault/tasks/tasks/eval_task.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 module TaskVault
-  class EvalTask < Task
-    attr_str :evaluation, default: '', serialize: true, always: true
+  module Tasks
+    class EvalTask < Task
+      attr_str :evaluation, default: '', serialize: true, always: true
 
-    alias code= evaluation=
-    alias code evaluation
+      alias code= evaluation=
+      alias code evaluation
 
-    protected
+      add_alias(:eval, :eval_task)
 
-    def run
-      eval(@evaluation)
+      protected
+
+      def run
+        eval(@evaluation)
+      end
     end
   end
 end
