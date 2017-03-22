409ba919279127f4d78460be4b90040eeda55a07
Brandon <d2sm10@hotmail.com>
2017-03-21 23:36:07 -0600

    Added new inventory component and item class.

---
+++ lib/components/inventory/item.rb
@@ -0,0 +1,61 @@
+module TaskVault
+  class Inventory < ServerComponent
+    class Item < BBLib::LazyClass
+      attr_str :key, serialize: true
+      attr_hash :description, default: [], serialize: true
+      attr_of Object, :value, default: nil, serialize: true
+      attr_of Time, :expiration, default: nil, allow_nil: true, serialize: true
+      attr_int :access_counter, default: 0
+      attr_of Time, :last_accessed, default: Time.now
+
+      after :_default_key, :lazy_init
+      after :access_update, :item, :value
+
+      alias_method :item, :value
+      alias_method :item=, :value=
+
+      def fits?(params)
+        return false unless params.is_a?(Hash) && params[:class]
+        params.all? do |k, param|
+          if k == :class
+            value.class.to_s == param.to_s
+          else
+            compare(param, description[k])
+          end
+        end
+      end
+
+      def compare(a, b)
+        case [a.class]
+        when [Regexp]
+          a =~ b.to_s
+        when [String]
+          a.to_s == b.to_s
+        else
+          a == b
+        end
+      end
+
+      def expired?
+        return false if expiration.nil?
+        Time.now <= expiration
+      end
+
+      protected
+
+      def access_update
+        self.access_counter += 1
+        self.last_accessed = Time.now
+      end
+
+      def _default_key
+        self.key = generate_key unless key
+      end
+
+      def generate_key
+        SecureRandom.hex(10)
+      end
+
+    end
+  end
+end
