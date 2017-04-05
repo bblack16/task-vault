936e28ceadae754138877fbc5dd2a7c1a3c1a326
Brandon <d2sm10@hotmail.com>
2017-04-04 23:01:02 -0600

    Added new task type: Spawner.

--- lib/components/vault/tasks/_tasks.rb
+++ lib/components/vault/tasks/_tasks.rb
@@ -2,9 +2,10 @@ require_relative 'task'
 require_relative 'tasks/cmd_task'
 require_relative 'tasks/script_dir'
 require_relative 'tasks/watch_folder'
-require_relative 'tasks/eval_task'
+require_relative 'tasks/eval'
 require_relative 'tasks/proc_task'
 require_relative 'tasks/sysmon'
+require_relative 'tasks/spawner'
 require_relative 'tasks/rabbitmq' if defined?(Bunny)
 require_relative 'tasks/mongo' if defined?(Mongo)
 require_relative 'tasks/elasticsearch' if defined?(RestClient)
