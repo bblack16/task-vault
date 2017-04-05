a0a8713899f9b54d188824cd17b2b37423922676
Brandon <d2sm10@hotmail.com>
2017-04-04 20:52:53 -0600

    Refactored to use RestClient::Resource.

--- lib/components/vault/tasks/tasks/elasticsearch.rb
+++ lib/components/vault/tasks/tasks/elasticsearch.rb
@@ -1,20 +1,16 @@
 module TaskVault
   module Tasks
     class Elasticsearch < Task
-      # attr_str :host, default: 'localhost', serialize: true, always: true
-      # attr_element_of [:http, :https], :protocol, default: :http, serialize: true, always: true
-      # attr_int :port, default: 9200, serialize: true, always: true
-      # attr_str :user, :pass, default: nil, allow_nil: true, serialize: true
-      attr_str :url, default: 'http://localhost:9200', serialize: true, always: true
-      attr_str :index, :type, default: nil, allow_nil:true, serialize: true, always: true
+      attr_str :index, :type, default: nil, allow_nil: true, serialize: true, always: true
       attr_ary_of Hash, :queries, default: [], serialize: true, add_rem: true
       attr_bool :cache, default: false, serialize: true, always: true
       attr_int :cache_limit, default: 5, serialize: true, always: true
+      attr_of RestClient::Resource, :client
 
       component_aliases(:elasticsearch, :elasticsearch_query)
 
       def connected?
-        RestClient.get(url)
+        client.get(url)
         true
       rescue => e
         queue_error(e)
@@ -28,7 +24,7 @@ module TaskVault
       protected
 
       def search_url
-        "#{url}/#{index ? "#{index}/#{type ? "#{type}/" : nil}" : nil}_search"
+        "/#{index ? "#{index}/#{type ? "#{type}/" : nil}" : nil}_search"
       end
 
       def run
@@ -39,7 +35,7 @@ module TaskVault
       end
 
       def run_query(query)
-        result = JSON.parse(RestClient.post(search_url, query.to_json).body)
+        result = JSON.parse(client.post(search_url, query.to_json).body)
         cache_result(result) if cache?
         process_result(result, query)
       rescue => e
