592990b6901cbd1468a9e2dd0c57695083dc6f50
Brandon <d2sm10@hotmail.com>
2017-04-04 20:57:02 -0600

    Added conditional requires for now until tasks are broken out into new gems or dependencies are handled a better way.

--- lib/components/vault/tasks/_tasks.rb
+++ lib/components/vault/tasks/_tasks.rb
@@ -4,9 +4,9 @@ require_relative 'tasks/script_dir'
 require_relative 'tasks/watch_folder'
 require_relative 'tasks/eval_task'
 require_relative 'tasks/proc_task'
-require_relative 'tasks/rabbitmq'
-require_relative 'tasks/mongo'
-require_relative 'tasks/elasticsearch'
-require_relative 'tasks/rest'
 require_relative 'tasks/sysmon'
-require_relative 'tasks/sql'
+require_relative 'tasks/rabbitmq' if defined?(Bunny)
+require_relative 'tasks/mongo' if defined?(Mongo)
+require_relative 'tasks/elasticsearch' if defined?(RestClient)
+require_relative 'tasks/rest' if defined?(RestClient)
+require_relative 'tasks/sql' if defined?(Sequel)
