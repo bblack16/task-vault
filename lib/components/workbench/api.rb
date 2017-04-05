2f3c9854aa4900da2d3244a9a9aa37a4d9b9d57a
Brandon <d2sm10@hotmail.com>
2017-04-04 22:58:56 -0600

    Cleaning up the API a bit. Still needs more work.

--- lib/components/workbench/api.rb
+++ lib/components/workbench/api.rb
@@ -11,48 +11,32 @@ module TaskVault
       end
 
       get '/recipes/:id' do
-        component.recipes[params[:id].to_sym]
+        if params[:id] =~ /\d+/
+          component.recipe(params[:id].to_i)
+        else
+          { status: 404, message: 'Invalid ID format. Expected an integer.' }
+        end
       end
 
       post '/recipes' do
         begin
           component.add(params.keys_to_sym)
         rescue => e
-          { error: e }
+          { status: 500, message: "Failed to add recipe: #{e}" }
         end
       end
 
       post '/recipes/remove/:id' do
-        if params[:id].nil? || component.recipes[params[:id].to_sym].nil?
-          { success: false, request: :remove, message: 'You must pass a valid task id.' }
+        if params[:id] =~ /\d+/
+          component.remove(params[:id])
         else
-          { success: component.remove(params[:id].to_sym), request: :cancel }
+          { status: 404, message: 'Invalid ID format. Expected an integer.' }
         end
       end
 
       # delete '/recipes/:id' do
-      #   if params[:id].nil? || component.recipes[params[:id].to_sym].nil?
-      #     { success: false, request: :delete, message: 'You must pass a valid recipe name.' }
-      #   else
-      #     { success: !component.delete(params[:id].to_sym).empty?, request: :delete, id: params[:id] }
-      #   end
+      #   component.delete(params[:id])
       # end
-
-      put '/load' do
-        if params[:path]
-          component.load_recipes(params[:path])
-        else
-          component.load_recipes
-        end
-      end
-      
-      put '/recipes/:id/save' do
-        component.save(params[:id].to_sym, format: (params[:format] || :yaml).to_sym)
-      end
-
-      put 'recipes/save_all' do
-        component.save_all(format: (params[:format] || :yaml).to_sym)
-      end
     end
   end
 end
