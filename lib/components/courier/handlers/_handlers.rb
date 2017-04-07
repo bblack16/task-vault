b29769472db056a4cb526482f5257bf1edf3c3b7
bj002440 <bblack@digitalglobe.com>
2017-04-07 11:36:33 -0600

    Modified to pull in dependencies only if they exist.

--- lib/components/courier/handlers/_handlers.rb
+++ lib/components/courier/handlers/_handlers.rb
@@ -1,8 +1,8 @@
 require_relative 'message_handler'
 require_relative 'task_vault_handler'
 require_relative 'logger'
-require_relative 'mongodb'
-require_relative 'rabbitmq'
 require_relative 'logstash'
-require_relative 'rest'
 require_relative 'tasker'
+require_relative 'mongodb' if defined?(Mongo)
+require_relative 'rabbitmq' if defined?(Bunny)
+require_relative 'rest' if defined?(RestClient)
