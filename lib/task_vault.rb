e2929a07f0d81a8bac1c15e9fba2692f1fc6e967
Brandon Black <d2sm10@hotmail.com>
2018-09-20 22:52:02 -0600

    Hardcoded requires and added util.

--- lib/task_vault.rb
+++ lib/task_vault.rb
@@ -2,20 +2,18 @@ require 'bblib' unless defined?(BBLib::VERSION)
 require 'securerandom'
 
 require_relative 'task_vault/version'
-
+require_relative 'task_vault/util'
 require_relative 'task_vault/puts_override'
 require_relative 'task_vault/runnable'
-
 require_relative 'task_vault/task'
 require_relative 'task_vault/components/overseer'
 require_relative 'task_vault/components/courier'
 require_relative 'task_vault/components/workbench'
 require_relative 'task_vault/server'
 
-BBLib.scan_files(File.expand_path('../task_vault/tasks', __FILE__), '*.rb') do |file|
-  require_relative file
-end
+require_relative 'task_vault/tasks/cmd'
+require_relative 'task_vault/tasks/eval'
+require_relative 'task_vault/tasks/proc'
+require_relative 'task_vault/tasks/watch_folder'
 
-BBLib.scan_files(File.expand_path('../task_vault/specialty_tasks', __FILE__), '*.rb') do |file|
-  require_relative file
-end
+require_relative 'task_vault/specialty_tasks/hand_brake_watch_folder'
